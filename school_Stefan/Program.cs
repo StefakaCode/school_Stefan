347d82f896786deb0204314bc62562cbb9c8f07c
Tonziss <antonatanasov2002@gmail.com>
2025-02-14 12:21:43 +0200

    add Print method

--- school_Stefan/Program.cs
+++ school_Stefan/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace school_Stefan
+﻿using System.Security.Principal;
+
+namespace school_Stefan
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            Print();
+        }
+
+        static void Print() {
+            Console.WriteLine(Console.ReadLine());
         }
     }
 }
