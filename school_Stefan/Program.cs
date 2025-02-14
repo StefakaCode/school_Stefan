35150b390a9b4d9b82d0e482bcdf9bc90edcc849
Nikito69 <nikiniki2777@gmail.com>
2025-02-14 12:03:32 +0200

    Add project files.

---
+++ school_Stefan/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace school_Stefan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
