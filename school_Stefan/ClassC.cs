beb59821e70b23497fe39ad1384339041eb425d1
Tonziss <antonatanasov2002@gmail.com>
2025-02-21 11:18:40 +0200

    add class c function sqrt

---
+++ school_Stefan/ClassC.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace school_Stefan
+{
+    internal class ClassC
+    {
+        public double Sqrt(double a)
+        {
+            return Math.Sqrt(a);
+            //
+        }
+    }
+}
